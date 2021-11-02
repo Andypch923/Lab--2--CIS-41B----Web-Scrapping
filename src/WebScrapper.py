cc17c90f5f7c36ef1fe245ace384001773abb926
Andypch923 <andypoon923@gmail.com>
2021-11-02 17:57:33 +0800

    Update WebScrapper.py

--- src/WebScrapper.py
+++ src/WebScrapper.py
@@ -7,7 +7,7 @@ import pandas as pd
 myUrl = "https://en.wikipedia.org/wiki/List_of_countries_by_carbon_dioxide_emissions"
 uClient = uReq(myUrl)
 page_html = uClient.read()
-page_soup = bs(page_html,"html.parser")
+page_soup = bs(page_html,"lxml")
 
 co2_dataFrame = {
     "Country":[],
@@ -23,7 +23,13 @@ co2_dataFrame = {
 }
 
 
-co2table = page_soup.findAll("table",{"class":"wikitable sortable"})
-print(co2table[0])
+co2table = page_soup.find("table",{"class":"wikitable sortable"})
 
+headers = []
 
+for i in co2table.find_all('th'):
+    title = i.text.strip()
+    headers.append(title)
+
+for i in co2table.find_all('tr'):
+    print(i.text)
