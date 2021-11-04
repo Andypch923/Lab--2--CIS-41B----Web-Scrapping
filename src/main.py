aa674c7084c5f56b6585ff23e16d47ff2b13285d
Andypch923 <andypoon923@gmail.com>
2021-11-05 06:32:50 +0800

    Delete main.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import WebScrapper
-import DataBase
-
-temp = DataBase.DataBase()
-sth = WebScrapper.TemperatureScraper()
-
-temp.createTable(sth.getlistOfCO2Data())
-for i in temp.fetch2017top10Countries():
-    print(i)
